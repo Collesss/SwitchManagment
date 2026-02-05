2b0660f27ddee4e4945a9ce895869e493dcfa8cf
Лесиков Сергей Игоревич <lesikov@office.crocusgroup.ru>
2026-02-05 18:36:14 +0300

    update

--- SwitchManagment.API.SwitchService/Implementations/SwitchServiceHPComware5.cs
+++ SwitchManagment.API.SwitchService/Implementations/SwitchServiceHPComware5.cs
@@ -14,6 +14,7 @@ namespace SwitchManagment.API.SwitchService.Implementations
         //test login: admin3 ; pass: 1234
         public async Task SettingPort(string ipOrName, string login, string password, string superPassword, string interfaceName, bool isTrunk, CancellationToken cancellationToken = default, params int[] vlans)
         {
+            #region validation
             ArgumentException.ThrowIfNullOrWhiteSpace(ipOrName);
             ArgumentException.ThrowIfNullOrWhiteSpace(login);
             ArgumentNullException.ThrowIfNull(password);
@@ -21,60 +22,47 @@ namespace SwitchManagment.API.SwitchService.Implementations
             ArgumentNullException.ThrowIfNull(vlans);
 
             if (vlans.Length == 0)
-                throw new ArgumentException("vlans cant be em", nameof(vlans));
+                throw new ArgumentException("VLAN list cant be empty.", nameof(vlans));
             else if (!isTrunk && vlans.Length != 1)
-                throw new ArgumentException("if port not trunck, vlan can be contains only one value.", nameof(vlans));
+                throw new ArgumentException("If the port is not a trunk, the VLAN list must contain only one value.", nameof(vlans));
 
             if (!vlans.All(vlan => vlan > 0))
-                throw new ArgumentException("vlan cant be less 1.", nameof(vlans));
+                throw new ArgumentException("VLAN cant be less 1.", nameof(vlans));
 
-            //if(vlans.All(vlan => vlans.Any(vl => vlan == vl)))
+            if(vlans.Any(vlan => vlans.Count(vl => vlan == vl) > 1))
+                throw new ArgumentException("VLAN list can be unique.", nameof(vlans));
+            #endregion
 
+            using var sshClient = new SshClient(ipOrName, 22, login, password);
+            
+            await sshClient.ConnectAsync(cancellationToken);
 
-            using (var sshClient = new SshClient(ipOrName, 22, login, password))
+            using ShellStream shellStream = sshClient.CreateShellStreamNoTerminal();
+
+            shellStream.WriteLine("_cmdline-mode on");
+            shellStream.WriteLine("Y");
+            shellStream.WriteLine(superPassword);
+            shellStream.WriteLine("system-view");
+            shellStream.WriteLine($"interface {interfaceName}");
+
+            if (isTrunk)
             {
-                sshClient.Connect();
-
-
-                await sshClient.ConnectAsync(CancellationToken.None);
-
-                ShellStream shellStream = sshClient.CreateShellStreamNoTerminal();
-
-                shellStream.WriteLine("_cmdline-mode on");
-                shellStream.WriteLine("Y");
-                shellStream.WriteLine("512900");
-                shellStream.WriteLine("system-view");
-                shellStream.WriteLine($"interface {interfaceName}");
-
-                if(isTrunk)
-                {
-                    shellStream.WriteLine("port link-type access");
-                    shellStream.WriteLine("port link-type trunk");
-                    shellStream.WriteLine($"port trunk permit vlan {string.Join(' ', vlans)}");
-
-                    Console.WriteLine(shellStream.Expect("Done."));
-                }
-                else
-                {
-                    shellStream.WriteLine("port link-type access");
-                    shellStream.WriteLine($"port access permit vlan {vlans.Single()}");
-                }
+                shellStream.WriteLine("port link-type access");
+                shellStream.WriteLine("port link-type trunk");
+                shellStream.WriteLine($"port trunk permit vlan {string.Join(' ', vlans)}");
 
                 Console.WriteLine(shellStream.Expect("Done."));
-
-                /*
-                while (shellStream.DataAvailable)
-                {
-                    if (shellStream.ReadLine() is string line)
-                        Console.WriteLine(line);
-                    else
-                        break;
-                }
-                */
-
-                shellStream.Close();
-                sshClient.Disconnect();
             }
+            else
+            {
+                shellStream.WriteLine("port link-type access");
+                shellStream.WriteLine($"port access permit vlan {vlans.Single()}");
+            }
+
+            Console.WriteLine(shellStream.Expect("Done."));
+
+            shellStream.Close();
+            sshClient.Disconnect();
         }
     }
 }
